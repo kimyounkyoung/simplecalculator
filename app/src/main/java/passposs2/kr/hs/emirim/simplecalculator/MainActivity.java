b779819eb1c3287d0f8b3dd7d9687fb948081f06
kimyounkyoung <passposs2@naver.com>
2017-03-28 20:17:21 +0900

    #2 simple calculator

--- app/src/main/java/passposs2/kr/hs/emirim/simplecalculator/MainActivity.java
+++ app/src/main/java/passposs2/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -2,12 +2,114 @@ package passposs2.kr.hs.emirim.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
+    EditText editFirst, editSecond;
+    TextView textResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        editFirst=(EditText)findViewById(R.id.edit_first);
+        editSecond=(EditText)findViewById(R.id.edit_second);
+        Button butPlus=(Button)findViewById(R.id.but_plus);
+        Button butMinus=(Button)findViewById(R.id.but_minus);
+        Button butMultiply=(Button)findViewById(R.id.but_multply);
+        Button butDivision=(Button)findViewById(R.id.but_division);
+        textResult=(TextView)findViewById(R.id.text_result);
+        butPlus.setOnClickListener(butHandler);
+        butMinus.setOnClickListener(butHandler);
+        butMultiply.setOnClickListener(butHandler);
+        butDivision.setOnClickListener(butHandler);
+
+        /*
+        butPlus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int num1=Integer.parseInt(editFirst.getText().toString());
+                int num2=Integer.parseInt(editSecond.getText().toString());
+                textResult.setText(num1+num2+"");
+            }
+        });
+        butMinus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int num1=Integer.parseInt(editFirst.getText().toString());
+                int num2=Integer.parseInt(editSecond.getText().toString());
+                textResult.setText(num1-num2+"");
+            }
+        });
+        butMultiply.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int num1=Integer.parseInt(editFirst.getText().toString());
+                int num2=Integer.parseInt(editSecond.getText().toString());
+                textResult.setText(num1*num2+"");
+            }
+        });
+        butDivision.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int num1=Integer.parseInt(editFirst.getText().toString());
+                int num2=Integer.parseInt(editSecond.getText().toString());
+                textResult.setText(num1/num2+"");
+            }
+        });
+        */
+    }   //end oncilck
+
+    View.OnClickListener butHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int num1=Integer.parseInt(editFirst.getText().toString());
+            int num2=Integer.parseInt(editSecond.getText().toString());
+            int result=0;
+
+            switch (v.getId()){
+                case R.id.but_plus:
+                    result=num1+num2;
+                    break;
+                case R.id.but_minus:
+                    result=num1-num2;
+                    break;
+                case R.id.but_multiply:
+                    result=num1*num2;
+                    break;
+                case R.id.but_division:
+                    result=num1/num2;
+                    break;
+            }
+
+            textResult.setText(result+"");
+        }
+    };
+
+    /*
+    @Override
+    public void onClick(View view) {
+        int num1=Integer.parseInt(editFirst.getText().toString());
+        int num2=Integer.parseInt(editSecond.getText().toString());
+        int result=0;
+
+        switch (view.getId()){
+            case R.id.but_plus:
+                result=num1+num2;
+                break;
+            case R.id.but_minus:
+                result=num1-num2;
+                break;
+            case R.id.but_multiply:
+                result=num1*num2;
+                break;
+            case R.id.but_division:
+                result=num1/num2;
+                break;
+        }
+
+        textResult.setText(result+"");
+    }*/
 }
